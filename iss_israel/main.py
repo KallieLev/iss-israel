e1c70b9e849ec25f723b35ba53ba108a8690b9cf
KallieLevy <kali.levy1@gmail.com>
2020-03-14 18:10:12 +0200

    fixed function calling

--- iss_israel/main.py
+++ iss_israel/main.py
@@ -2,4 +2,4 @@ from iss_israel.retrieve_iss_info import RetrieveISSInfo
 
 if __name__ == '__main__':
     retrieve_info = RetrieveISSInfo()
-    retrieve_info.insert_cities_info_postgre()
+    retrieve_info.save_cities_info()
