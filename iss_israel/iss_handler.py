a143e8b1b0f4e8dc3171c6d46ea3fdad22c371de
KallieLevy <kali.levy1@gmail.com>
2020-03-14 17:29:01 +0200

    added hanlder for iss api

---
+++ iss_israel/iss_handler.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+with open('../config.json', 'r') as config_file:
+    cfg = json.load(config_file)
+
+
+class ISSHandler:
+    def __init__(self):
+        self.url = cfg['iss']['url']
+
+    def get_timestamps(self, latitude, longitude, number=5):
+        url = self.url.format(lat=latitude, lon=longitude, n=number)
+        resp = requests.get(url).json()['response']
+        return resp
