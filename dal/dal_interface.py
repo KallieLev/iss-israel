d9dbfa985fc69cdb17e0e690aa902a499452d4a2
KallieLevy <kali.levy1@gmail.com>
2020-03-15 14:08:12 +0200

    made interface abstract

--- dal/dal_interface.py
+++ dal/dal_interface.py
@@ -1,3 +1,7 @@
-class DalInterface:
+from abc import ABC, abstractmethod
+
+
+class DalInterface(ABC):
+    @abstractmethod
     def insert_city(self, city_name, duration, rise_time, table_name):
         pass
