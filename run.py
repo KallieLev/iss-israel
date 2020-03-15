7b99fa74c37ad41eb8ce9ceb68073f739aaaf7f9
KallieLevy <kali.levy1@gmail.com>
2020-03-15 15:20:38 +0200

    fixed open config file

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import json
 from iss_israel.factory import initialize
 
 if __name__ == '__main__':
-    with open('../config.json', 'r') as config_file:
+    with open('config.json', 'r') as config_file:
         cfg = json.load(config_file)
 
     retrieve_info = initialize(cfg)
