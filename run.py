e1c01ae37d48f6593b95edbc2a1d4e4a800a5b55
KallieLevy <kali.levy1@gmail.com>
2020-03-15 15:23:33 +0200

    changed name of function

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import json
 
-from iss_israel.factory import initialize
+from iss_israel.factory import initialize_retriever
 
 if __name__ == '__main__':
     with open('config.json', 'r') as config_file:
         cfg = json.load(config_file)
 
-    retrieve_info = initialize(cfg)
+    retrieve_info = initialize_retriever(cfg)
     retrieve_info.save_cities_info()
